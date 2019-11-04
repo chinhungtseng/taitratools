076ecab020814ce5594b873ffd35fae9f0733182
unknown <chinhung@taitra.org.tw>
2019-11-04 13:30:16 +0800

    fixed str2regex

--- R/string.R
+++ R/string.R
@@ -3,6 +3,7 @@
 str2regex <- function(x, sep = ",", sub = 1e2, start = "^", end = "") {
   tmp <- trimws(unlist(strsplit(x, sep)))
   tmp <- unique(substr(tmp, 1, sub))
+  tmp <- tmp[tmp != ""]
   paste0(start, tmp, end, collapse = "|")
 }
 

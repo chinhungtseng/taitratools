60c04e94b0cda2ddf5199bf94e72fef95bad3933
chinhungtseng <bluesshown@gmail.com>
2020-11-06 13:51:42 +0800

    Update package's data

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,15 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Welcome to taitratools package")
-  packageStartupMessage("Checking your package's version.")
-  pkg_update <- check_pkg_update()
-
-  if (pkg_update == -1) {
-    packageStartupMessage("API rate limit exceeded")
-  } else if (pkg_update) {
-    packageStartupMessage("-> New version is released.")
-    packageStartupMessage("-> please updgrade your taitatools package using below script:\n")
-    packageStartupMessage('---> devtools::install_github("chinhungtseng/taitratools")\n')
-  } else {
-    packageStartupMessage("-> Already up to date.")
-  }
+  # packageStartupMessage("Checking your package's version.")
+  # pkg_update <- check_pkg_update()
+  #
+  # if (pkg_update == -1) {
+  #   packageStartupMessage("API rate limit exceeded")
+  # } else if (pkg_update) {
+  #   packageStartupMessage("-> New version is released.")
+  #   packageStartupMessage("-> please updgrade your taitatools package using below script:\n")
+  #   packageStartupMessage('---> devtools::install_github("chinhungtseng/taitratools")\n')
+  # } else {
+  #   packageStartupMessage("-> Already up to date.")
+  # }
 }
